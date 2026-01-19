3b4c4a1b356bdfe748928bfda92da45e5e0aef81
Noname <you@example.com>
2026-01-19 11:11:52 +0900

    Initial Commit

---
+++ DeepLocal/DeepLocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLocalApp.swift
+//  DeepLocal
+//
+//  Created by warpflow on 2026/01/19.
+//
+
+import SwiftUI
+
+@main
+struct DeepLocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
